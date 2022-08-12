7d16ba5c0dffd0053526470ec98833814ef73652
Aaris-Kazi <aariskazi@gmail.com>
2022-08-12 21:12:26 +0530

    changed details

--- main/models.py
+++ main/models.py
@@ -6,6 +6,7 @@ class hotel_details(models.Model):
     location = models.CharField(max_length=50)
     hotel_image = models.CharField(max_length=150)
     hotel_name = models.CharField(max_length=50)
+    hotel_type = models.CharField(max_length=50, default='hotel')
     price = models.IntegerField()
     def __str__(self):
         return str(self.location)+" "+str(self.hotel_image)+" "+str(self.hotel_name)+" "+str(self.price)
