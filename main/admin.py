cdd590a10197381c53f718f31ba2c1adfa3c3a58
Aaris-Kazi <aariskazi@gmail.com>
2022-08-12 17:46:27 +0530

    models and admin

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from . import models
 # Register your models here.
+
+class Hotel(admin.ModelAdmin):
+    list_display = ['location', 'hotel_name', 'price']
+    ordering= ['location']
+
+admin.site.register(models.hotel_details, Hotel)
--- main/models.py
+++ main/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 from django.conf import settings
 # Create your models here.
+
+class hotel_details(models.Model):
+    location = models.CharField(max_length=50)
+    hotel_image = models.CharField(max_length=150)
+    hotel_name = models.CharField(max_length=50)
+    price = models.IntegerField()
+    def __str__(self):
+        return str(self.location)+" "+str(self.hotel_image)+" "+str(self.hotel_name)+" "+str(self.price)
