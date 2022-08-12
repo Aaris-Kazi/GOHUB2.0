e66b7fc3346455ee89fd149e7883c2db42b0c9ae
Aaris-Kazi <aariskazi@gmail.com>
2022-08-12 20:45:17 +0530

    now mysql connected

--- gohub/settings.py
+++ gohub/settings.py
@@ -76,9 +76,20 @@ WSGI_APPLICATION = 'gohub.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'gohub',
+        'USER': 'root',
+        'PASSWORD': '12345',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+        'OPTIONS': {  
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"  
+        } 
     }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
 }
 
 
