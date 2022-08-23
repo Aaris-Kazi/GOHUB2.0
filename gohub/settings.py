8d03bb8bbce9ddc79716598ce76e2dea55279897
Aaris-Kazi <aariskazi@gmail.com>
2022-08-24 00:39:23 +0530

    back to mysql

--- gohub/settings.py
+++ gohub/settings.py
@@ -75,27 +75,25 @@ WSGI_APPLICATION = 'gohub.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.mysql',
-    #     'NAME': 'gohub',
-    #     'USER': 'root',
-    #     'PASSWORD': '12345',
-    #     'HOST': '127.0.0.1',
-    #     'PORT': '3306',
-    #     'OPTIONS': {  
-    #         'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"  
-    #     } 
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
+        'ENGINE': 'django.db.backends.mysql',
         'NAME': 'gohub',
-        'USER': 'postgres',
+        'USER': 'root',
         'PASSWORD': '12345',
-        'HOST': 'localhost',
-        'PORT': '5432',
-        # 'OPTIONS': {  
-        #     'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"  
-        # } 
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+        'OPTIONS': {  
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"  
+        } 
     }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.postgresql',
+    #     'NAME': 'gohub',
+    #     'USER': 'postgres',
+    #     'PASSWORD': '12345',
+    #     'HOST': 'localhost',
+    #     'PORT': '5432',
+    # }
     # 'default': {
     #     'ENGINE': 'django.db.backends.sqlite3',
     #     'NAME': BASE_DIR / 'db.sqlite3',
