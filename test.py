f57a4dc2ce5ad50612fc19e36e08d629bce83374
Aaris-Kazi <aariskazi@gmail.com>
2022-08-27 20:47:23 +0530

    database changes

--- test.py
+++ test.py
@@ -37,11 +37,12 @@ cursor = sqlite3.Cursor(conn)
 
 # df = pd.read_csv('main_resultnotfound.csv', sep=';')
 # df = pd.read_csv('main_hotel_details.csv', sep=';')
-df = pd.read_csv('main_hotel_booking.csv', sep=';')
+# df = pd.read_csv('main_hotel_booking.csv', sep=';')
+df = pd.read_csv('auth_user.csv', sep=';')
 
 # df.to_sql('main_resultsnotfound', conn, if_exists='replace', index=False)
 # df.to_sql('main_hotel_details', conn, if_exists='replace', index=False)
-df.to_sql('main_hotel_booking', conn, if_exists='replace', index=False)
+df.to_sql('auth_user', conn, if_exists='replace', index=False)
 
 # query = 'INSERT INTO main_resultsnotfound (location) VALUES (?)'
 # cursor.executemany(query, contents)
