d1763dd4f3367fb6d00ca02f60470170d32e517a
Aaris-Kazi <aariskazi@gmail.com>
2022-08-30 12:36:44 +0530

    for cpp

--- test.py
+++ test.py
@@ -29,23 +29,44 @@
 #     i = i.strip("₹")
 #     price.append()
 
-import sqlite3
-import pandas as pd
+# import sqlite3
+# import pandas as pd
 
-conn = sqlite3.connect('db.sqlite3')
-cursor = sqlite3.Cursor(conn)
-
+# conn = sqlite3.connect('db.sqlite3')
+# cursor = sqlite3.Cursor(conn)
+# df = pd.read_csv('auth_user.csv', sep=';')
+# df.to_sql('auth_user', conn, if_exists='replace', index=False)
+#########################################################################
 # df = pd.read_csv('main_resultnotfound.csv', sep=';')
 # df = pd.read_csv('main_hotel_details.csv', sep=';')
 # df = pd.read_csv('main_hotel_booking.csv', sep=';')
-df = pd.read_csv('auth_user.csv', sep=';')
 
 # df.to_sql('main_resultsnotfound', conn, if_exists='replace', index=False)
 # df.to_sql('main_hotel_details', conn, if_exists='replace', index=False)
-df.to_sql('auth_user', conn, if_exists='replace', index=False)
 
 # query = 'INSERT INTO main_resultsnotfound (location) VALUES (?)'
 # cursor.executemany(query, contents)
 # cursor.executemany(query, df)
 # conn.commit()
 # conn.close()
+##############################
+
+text = "un was establish on 24-10-1945, India got freedon 15-09-1945."
+# text = "something something something on 24-10-1947, India got freedon 15-09-1945."
+text = input()
+temp = 0
+count = 0
+sp_text = text.split(' ')
+for i in sp_text:
+    i = i.replace(',', '')
+    i = i.replace('.', '')
+    if '-' in i:
+        # print(i)
+        i = i.split('-')
+        # print(i[len(i)-1])
+        if temp == int(i[len(i)-1]):
+            pass
+        else:
+            temp = int(i[len(i)-1])
+            count+=1
+print(count)
