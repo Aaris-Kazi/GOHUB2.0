4f5ad79695777e6bda41420a29ba47a3029b9a29
Aaris-Kazi <aariskazi@gmail.com>
2022-10-04 22:02:24 +0530

    Getting import export installed

--- gohub/settings.py
+++ gohub/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     ################################
     "whitenoise.runserver_nostatic",
+    'import_export',
     ##################################
     "main"
 ]
--- main/admin.py
+++ main/admin.py
@@ -1,14 +1,15 @@
 from django.contrib import admin
 from . import models
+from import_export.admin import ImportExportModelAdmin
 # Register your models here.
 
-class Hotel(admin.ModelAdmin):
+class Hotel(ImportExportModelAdmin, admin.ModelAdmin):
     list_display = ['location', 'hotel_name', 'hotel_type', 'price']
     ordering= ['location']
 
 admin.site.register(models.hotel_details, Hotel)
 
-class Booking(admin.ModelAdmin):
+class Booking(ImportExportModelAdmin, admin.ModelAdmin):
     list_display = ['userid', 'hotelid', 'location', 'startday', 'endday', 'price']
     ordering= ['id']
 
